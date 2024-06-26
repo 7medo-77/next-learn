cdb1f59e890ee53e289650d7c16a7e7faba4df20
hmedo <hmedo@7medo>
2024-06-26 17:24:37 +0300

    _17-24

--- app/ui/fonts.ts
+++
@@ -1,8 +0,0 @@
-import { Lusitana, Inter } from 'next/font/google';
-
-export const inter = Inter({ subsets: ['latin'] });
-
-export const lusi = Lusitana({
-	weight: [ '400', '700'],
-	subsets: ['latin'],
-});
